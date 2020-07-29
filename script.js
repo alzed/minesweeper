d725d69e399be30c1220f03a24ab592eec81879d
Madhumitha P Ravishankar <madhumithap.ravishankar@gmail.com>
2020-07-29 17:44:07 +0000

    Resize and center the canvas

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let cnv;
+
+function centerCanvas() {
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  cnv = createCanvas(300, 300);
+  centerCanvas();
+  background(50);
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function draw() {
